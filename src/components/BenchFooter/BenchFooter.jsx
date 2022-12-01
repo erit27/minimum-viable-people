e596c1d99c2dd6cb50c62fe0c675b8eb9773c151
Eilidh Ritchie <eilidh.ritchie@gmail.com>
2022-12-01 04:59:20 -0800

    add footer to /book and /create acct

---
+++ src/components/BenchFooter/BenchFooter.jsx
@@ -0,0 +1,11 @@
+import './BenchFooter.scss';
+import benchLogo from '../../assets/logos/bench_logo.jpeg';
+import { Link } from "react-router-dom";
+
+export default function BenchFooter() {
+  return (
+    <>
+
+    </>
+  )
+}
