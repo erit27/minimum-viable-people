845c065e724125d9892bddcbc8332c3add437192
geet <geeti_hashemi@yahoo.com>
2022-11-30 22:46:34 -0800

    Create CreateAccount page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss';
+import CreateAccount from './pages/CreateAccount/CreateAccount';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <CreateAccount />
       </header>
     </div>
   );
--- src/pages/CreateAccount/CreateAccount.jsx
+++ src/pages/CreateAccount/CreateAccount.jsx
@@ -1,10 +1,33 @@
 import '../../pages/CreateAccount/CreateAccount.scss';
+import Logo from '../../assets/logos/bench_logo.jpeg';
+import Image from '../../assets/image/lars-full.jpg';
 
 function CreateAccount (){
   return (
-    <div>
-      
+    <div className='account'>
+      <div className='account-main'>
+        <img className='account-logo' src={Logo} alt="logo" />
+        <h1 className='account-title'>Create Your Bench account</h1>
+        <p className='account-paragraph'>Review your account details and create a password.</p>
+        <form className='account-form'>
+          <label className='account-name'>FIRST NAME</label>
+          <input className='account-input' type="text" />
+          <label className='account-lastName'>LAST NAME</label>
+          <input className='account-input'type="text" />
+          <label className='account-email'>EMAIL</label>
+          <input className='account-input' type="text" placeholder='test@test.com'/>
+          <label className='account-password'>PASSWORD</label>
+          <input className='account-input'type="text" />
+        </form>
+        <p className='account-agreement'>By signing up you accept our <span> End User License Agreement And Terms of Service.</span>
+        </p>
+      </div>
+      <div className='account-img'>
+        <img className='account-img__lars' src={Image} alt="larsImage" />
+      </div>
     </div>
+      // <button>NEXT</button>
+    
   )
 }
 
