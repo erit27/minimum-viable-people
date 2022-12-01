114fce0faaf78dcd2b63e6f0c7699469797ce608
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-12-01 00:51:42 -0800

    styling

--- src/pages/CompanyDetails/CompanyDetails.jsx
+++ src/pages/CompanyDetails/CompanyDetails.jsx
@@ -5,69 +5,69 @@ function CompanyDetails() {
     return (<>
         <div className='container'>
             <div className='container-large'>
-                <div className='company-details'>
-                    <img src={logo} alt="Bench Company Logo" className='company-details__logo' />
+                <div className='container-medium'>
 
-                    <h1 className='company-details__title'>Tell us about your business</h1>
-                    <p className='company-details__description'>We'd like to learn more about your business so that we can provide you with the best experience possible.</p>
-                    <h2 className='company-details__sub'>COUNTRY</h2>
-                    <p>Select country your business is registered in</p>
-                    <div>
-                        <select>
-                            <option>America</option>
-                            <option>Canada</option>
-                        </select>
+                    <div className='company-details'>
+                        <img src={logo} alt="Bench Company Logo" className='company-details__logo' />
+
+                        <h1 className='company-details__title'>Tell us about your business</h1>
+                        <p className='company-details__description'>We'd like to learn more about your business so that we can provide you with the best experience possible.</p>
+                        <h2 className='company-details__sub'>COUNTRY</h2>
+                        <p className='company-details__sub-info'>Select country your business is registered in</p>
+                        <div>
+                            <select className='company-details__selector'>
+                                <option>America</option>
+                                <option>Canada</option>
+                            </select>
+                        </div>
+                        <h2 className='company-details__sub'>COMPANY SIZE</h2>
+                        <p className='company-details__sub-info'>Select number of owners, employees, and contractors</p>
+                        <div>
+                            <select className='company-details__selector'>
+                                <option>1</option>
+                                <option>2-5</option>
+                                <option>6-10</option>
+                                <option>11-25</option>
+                                <option>25+</option>
+                                <option>I'm not sure</option>
+                            </select>
+                        </div>
+                        <h2 className='company-details__sub'>MONTHLY REVENUE</h2>
+                        <p className='company-details__sub-info'>Select average monthly revenue before deductions</p>
+                        <div>
+                            <select className='company-details__selector'>
+                                <option>$0 (Pre-revenue)</option>
+                                <option>$1-$1,000</option>
+                                <option>$1,000-$10,0000</option>
+                                <option>$10,000-$100,000</option>
+                                <option>More than $100,000</option>
+                                <option>I'm not sure</option>
+                            </select>
+                        </div>
+                        <h2 className='company-details__sub'>LAST TAX RETURN</h2>
+                        <p className='company-details__sub-info'>Select most recent business income tax filing</p>
+                        <div>
+                            <select className='company-details__selector'>
+                                <option>Tax year 2021</option>
+                                <option>Tax year 2020</option>
+                                <option>Tax year 2019 or prior</option>
+                                <option>My business is new</option>
+                                <option>I'm not sure</option>
+                            </select>
+                        </div>
                     </div>
-                    <h2 className='company-details__sub'>COMPANY SIZE</h2>
-                    <p>Select number of owners, employees, and contractors</p>
                     <div>
-                        <select>
-                            <option>1</option>
-                            <option>2-5</option>
-                            <option>6-10</option>
-                            <option>11-25</option>
-                            <option>25+</option>
-                            <option>I'm not sure</option>
-                        </select>
+                        <img src={lars} alt="" className='company-details__lars' />
                     </div>
-                    <h2 className='company-details__sub'>MONTHLY REVENUE</h2>
-                    <p>Select average monthly revenue before deductions</p>
-                    <div>
-                        <select>
-                            <option>$0 (Pre-revenue)</option>
-                            <option>$1-$1,000</option>
-                            <option>$1,000-$10,0000</option>
-                            <option>$10,000-$100,000</option>
-                            <option>More than $100,000</option>
-                            <option>I'm not sure</option>
-                        </select>
-                    </div>
-                    <h2 className='company-details__sub'>LAST TAX RETURN</h2>
-                    <p>Select most recent business income tax filing</p>
-                    <div>
-                        <select>
-                            <option>Tax year 2021</option>
-                            <option>Tax year 2020</option>
-                            <option>Tax year 2019 or prior</option>
-                            <option>My business is new</option>
-                            <option>I'm not sure</option>
-                        </select>
-                    </div>
-                    <footer>
-                        <button className='next-page'>NEXT</button>
-                    </footer>
                 </div>
 
 
-                <div className='company-details__lars'>
-                    <p className='company-details__lars-title'>Lars · Onboarding Specialist</p>
-                </div>
+
+                <footer className='next-page'>
+                    <button className='next-page__button'>NEXT &#8594;</button>
+                </footer>
             </div>
         </div>
-
-
-
-
     </>)
 }
 export default CompanyDetails
