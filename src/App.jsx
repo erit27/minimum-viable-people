52a9016d911dda805e4757781584deeb6cc8fda2
Eilidh Ritchie <eilidh.ritchie@gmail.com>
2022-11-30 20:55:20 -0800

    test text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoadingPage from './pages/LoadingPage/LoadingPage';
 
 function App() {
   return (
-    <div className="App">
-      <LoadingPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={LoadingPage}></Route>
+        <Route path='/signup-bench-free-month-bookkeeping' element={''}></Route>
+        <Route path='/company-details' element={''}></Route>
+        <Route path='/book' element={''}></Route>
+        <Route path='/create-account' element={''}></Route>
+        <Route path='/accounts'></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/LoadingPage/LoadingPage.jsx
+++ src/pages/LoadingPage/LoadingPage.jsx
@@ -1,11 +1,16 @@
 import './LoadingPage.scss';
 import BenchHeader from '../../components/BenchHeader/BenchHeader';
 import GetStarted from '../../components/GetStarted/GetStarted';
+import logo from '../../assets/logos/bench_logo.jpeg'
 
 export default function LoadingPage() {
   return (
     <>
       <BenchHeader />
+      <p className='test'>Hello</p>
+      <h1>TEST</h1>
+      <div>your text here</div>
+      <img src={logo} alt="" />
     </>
   )
 }
