99c93aaad5ba70723ea575351bf0973918bb30e3
Frank Qian <109840571+jiachen88@users.noreply.github.com>
2022-11-30 21:20:56 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss';
+import CompanyDetails from './CompanyDetails/CompanyDetails';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <CompanyDetails />
       </header>
     </div>
   );
